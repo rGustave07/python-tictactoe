4afb71c7e16c942254bda75e0c33f0fdb9ebf77f
Ritter Gustave <rGustave5@gmail.com>
2018-01-24 14:05:10 -0800

    Initialize structure

---
+++ main.py
@@ -0,0 +1,26 @@
+# tic tac toe game
+
+
+def menu(tryagain):
+    while tryagain:
+        # Define function that starts game
+        pass
+
+
+def print_board(selection, player):
+    top_line    = " _ | _ | _ "
+    middle_line = " _ | _ | _ "
+    bottom_line = "   |   |   "
+
+    print(top_line)
+    print(middle_line)
+    print(bottom_line)
+
+
+
+def player_decision():
+    print("X goes first (Choose 1-9): ")
+
+
+
+print_board('dummy', 'dummy')
