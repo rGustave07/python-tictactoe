b8d761a30b8a2630c55a131ae61e1bc6db4be88b
Ritter Gustave <rGustave5@gmail.com>
2018-01-25 12:46:19 -0800

    Check_win() incurs a bug

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ def menu(tryagain):
         row = int(input("Enter row selection: "))
         column = int(input("Enter column selection: "))
         player = input("Enter Mark: ")
+        print("\n")
 
         print_board(row, column, player)
         if check_win():
             print("Game Over")
+            retry = input("Would you like to try again? (True/False)")
+            if retry == 'False':
+                tryagain = False
 
 
 def print_board(row, column, player):
@@ -28,21 +32,21 @@ def print_board(row, column, player):
 
 
 def check_win():
-    if board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X':
+    if board[0][0] == 'X' or board[0][0] == 'O' and board[0][1] == 'X' or board[0][1] == 'O' and board[0][2] == 'X' or board[0][2] == 'O':
         return True
-    elif board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X':
+    elif board[1][0] == 'X' or board[1][0] == 'O' and board[1][1] == 'X' or board[1][1] == 'O' and board[1][2] == 'X' or board[1][2] == 'O':
         return True
-    elif board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X':
+    elif board[2][0] == 'X' or board[2][0] == 'O' and board[2][1] == 'X' or board[2][1] == 'O' and board[2][2] == 'X' or board[2][2] == 'O':
         return True
-    elif board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X':
+    elif board[0][0] == 'X' or board[0][0] == 'O' and board[1][0] == 'X' or board[1][0] == 'O' and board[2][0] == 'X' or board[2][0] == 'O':
         return True
-    elif board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X':
+    elif board[0][1] == 'X' or board[0][1] == 'O' and board[1][1] == 'X' or board[1][1] == 'O' and board[2][1] == 'X' or board[2][1] == 'O':
         return True
-    elif board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X':
+    elif board[0][2] == 'X' or board[0][2] == 'O' and board[1][2] == 'X' or board[1][2] == 'O' and board[2][2] == 'X' or board[2][2] == 'O':
         return True
-    elif board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
+    elif board[0][0] == 'X' or board[0][0] == 'O' and board[1][1] == 'X' or board[1][1] == 'O' and board[2][2] == 'X' or board[2][2] == 'O':
         return True
-    elif board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
+    elif board[0][2] == 'X' or board[0][2] == 'O' and board[1][1] == 'X' or board[1][1] == 'O' and board[2][0] == 'X' or board[2][0] == 'O':
         return True
     else:
         return False
