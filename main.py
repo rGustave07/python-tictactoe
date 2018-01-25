17c1872d348c5bfaaae49e291a4608ef296a2c4c
Ritter Gustave <rGustave5@gmail.com>
2018-01-25 12:24:03 -0800

    game now checks for win on X marker

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ board = [
     ['_', '_', '_']
 ]
 
+
 def menu(tryagain):
     while tryagain:
         # Define function that starts game
@@ -14,6 +15,9 @@ def menu(tryagain):
         player = input("Enter Mark: ")
 
         print_board(row, column, player)
+        if check_win():
+            print("Game Over")
+
 
 def print_board(row, column, player):
     board[row][column] = player
@@ -21,6 +25,27 @@ def print_board(row, column, player):
     print(board[0])
     print(board[1])
     print(board[2])
+
+
+def check_win():
+    if board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X':
+        return True
+    elif board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X':
+        return True
+    elif board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X':
+        return True
+    elif board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X':
+        return True
+    elif board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X':
+        return True
+    elif board[0][2] == 'X' and board[1][2] == 'X' and board[2][2] == 'X':
+        return True
+    elif board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X':
+        return True
+    elif board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X':
+        return True
+    else:
+        return False
 
 
 def player_decision():
